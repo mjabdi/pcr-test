fab44dcd795bc9191b27bb17302d8d61bae9c8be
unknown <m.jafarabdi@gmail.com>
2020-10-30 12:50:11 +0330

    Remove Stamp on Positive

--- src/pdf-creator.js
+++ src/pdf-creator.js
@@ -79,34 +79,41 @@ const createPDF = async (options , filename) =>
             doc.fillColor('grey').fontSize(9).font('Courier').text(`T +44 (0) 20 7935 4444 F +44 (0) 20 7486 3782 www.thelondonclinic.co.uk`, 50, 692, {characterSpacing : 1, wordSpacing : 1 , lineGap : 2 , align : 'center' }  );
             doc.fillColor('grey').fontSize(8).font('Courier').text(`Registered in England No 307579 Registered Charity No 211136`, 50, 708, {characterSpacing : 1, wordSpacing : 2 , lineGap : 1 , align : 'center' }  );
             
-            /// Draw the Stamp
-            const x = 300;
-            const y = 430;
-            const width = 200;
-            const height = 80;
-            color = 'grey';
-            const now = new Date();
-            const todayString = dateFormat(now, 'dd mmm yyyy').toUpperCase();
-            
-            doc.moveTo(x, y).lineTo(x + width, y).stroke(color); 
-            doc.moveTo(x , y).lineTo(x, y + height).stroke(color); 
-            doc.moveTo(x, y + height).lineTo(x + width, y + height).stroke(color);
-            doc.moveTo(x + width, y).lineTo(x + width, y + height).stroke(color);
-            
-            doc.fillColor(color).fontSize(11).font('Times-Roman').text(`RESULTS`, x + 5, y + 5, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 , underline : true });
-            doc.fillColor(color).fontSize(11).font('Times-Roman').text(`DATE RECEIVED`, x + 5, y + 18, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            doc.fillColor(color).fontSize(11).font('Times-Bold').text(`${todayString}`, x + 110, y + 18, {characterSpacing : 1, wordSpacing : 2 , lineGap : 1 });
-            doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Normal`, x + 5, y + 18 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            // doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Action:`, x + 5, y + 18 + 13 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Doctor:`, x + 5, y + 18 + 13 + 13 + 5 , {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            // doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Date:`, x + 5, y + 18 + 13 + 13 + 13 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Note Comments`, x + 5 + 65, y + 18 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-    
-            doc.image('assets/checkbox-tick2.png', x + 48, y + 18 + 12,  {scale: 0.38});
-            doc.image('assets/checkbox2.png', x + 158, y + 18 + 12,  {scale: 0.38});
-            doc.image('assets/signature.png', x + 70, y + 18 + 5 + 20 , {scale: 0.4});
-            //doc.fillColor(color).fontSize(11).font('Symbol').text(`☑`, x + 5 + 40, y + 150, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
-            ///-----------------
+         
+         
+            if (options.negative.toLowerCase() === 'negative')
+            {
+                /// Draw the Stamp
+                const x = 300;
+                const y = 430;
+                const width = 200;
+                const height = 80;
+                color = 'grey';
+                const now = new Date();
+                const todayString = dateFormat(now, 'dd mmm yyyy').toUpperCase();
+                
+                doc.moveTo(x, y).lineTo(x + width, y).stroke(color); 
+                doc.moveTo(x , y).lineTo(x, y + height).stroke(color); 
+                doc.moveTo(x, y + height).lineTo(x + width, y + height).stroke(color);
+                doc.moveTo(x + width, y).lineTo(x + width, y + height).stroke(color);
+                
+                doc.fillColor(color).fontSize(11).font('Times-Roman').text(`RESULTS`, x + 5, y + 5, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 , underline : true });
+                doc.fillColor(color).fontSize(11).font('Times-Roman').text(`DATE RECEIVED`, x + 5, y + 18, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+                doc.fillColor(color).fontSize(11).font('Times-Bold').text(`${todayString}`, x + 110, y + 18, {characterSpacing : 1, wordSpacing : 2 , lineGap : 1 });
+                doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Normal`, x + 5, y + 18 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+                // doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Action:`, x + 5, y + 18 + 13 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+                doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Doctor:`, x + 5, y + 18 + 13 + 13 + 5 , {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+                // doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Date:`, x + 5, y + 18 + 13 + 13 + 13 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+                doc.fillColor(color).fontSize(11).font('Times-Roman').text(`Note Comments`, x + 5 + 65, y + 18 + 13, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+        
+                doc.image('assets/checkbox-tick2.png', x + 48, y + 18 + 12,  {scale: 0.38});
+                doc.image('assets/checkbox2.png', x + 158, y + 18 + 12,  {scale: 0.38});
+                doc.image('assets/signature.png', x + 70, y + 18 + 5 + 20 , {scale: 0.4});
+                //doc.fillColor(color).fontSize(11).font('Symbol').text(`☑`, x + 5 + 40, y + 150, {characterSpacing : 0.8, wordSpacing : 1 , lineGap : 1 });
+               ///-----------------
+            }
+
+
       
             doc.end();
     
