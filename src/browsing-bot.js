4fa9b06486001bb5ad5655e1841d149ff5a43018
noori <82028072+noori1995@users.noreply.github.com>
2025-03-21 21:49:51 +0330

    fix

--- src/browsing-bot.js
+++ src/browsing-bot.js
@@ -45,17 +45,17 @@ module.exports =  async function (linkAdress) {
     browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto(linkAdress);
-    const cookieBannerSelector = "button.btn.btn-blue";
-    const button = await page.$(cookieBannerSelector);
-    if (button) {
-      console.log('Cookie banner found. Clicking the "I understand" button...');
-      printPageText(page, '1');
-      await button.click();
-      console.log("Redirected to:", page.url());
-    } else {
-      console.log("Cookie banner not found.");
-    }
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    // const cookieBannerSelector = "button.btn.btn-blue";
+    // const button = await page.$(cookieBannerSelector);
+    // if (button) {
+    //   console.log('Cookie banner found. Clicking the "I understand" button...');
+    //   printPageText(page, '1');
+    //   await button.click();
+    //   console.log("Redirected to:", page.url());
+    // } else {
+    //   console.log("Cookie banner not found.");
+    // }
+    // await new Promise((resolve) => setTimeout(resolve, 10000));
 
     // Get only the visible text from the page
     printPageText(page, '2');
